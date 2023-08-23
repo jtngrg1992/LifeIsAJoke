873b25df449c2544a4c340b861a3afac26e3e4bc
Jatin Garg <jtngrg1992@hotmail.com>
2023-08-24 00:16:53 +0530

    worker for fetching a single joke

---
+++ LifeIsAJoke/Services/JokeFetcher.swift
@@ -0,0 +1,8 @@
+//
+//  JokeFetcher.swift
+//  LifeIsAJoke
+//
+//  Created by Jatin Garg on 24/08/23.
+//
+
+import Foundation
---
+++ LifeIsAJoke/Utilities/API.swift
@@ -0,0 +1,8 @@
+//
+//  API.swift
+//  LifeIsAJoke
+//
+//  Created by Jatin Garg on 24/08/23.
+//
+
+import Foundation
